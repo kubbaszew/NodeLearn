79de72407ba3802f0e0f3ada6de8139c9e0c6c8a
kubbaszew <kubbaszew@gmail.com>
2016-09-09 21:28:32 +0200

    dalej ucze sie

---
+++ htttp_r.js
@@ -0,0 +1,10 @@
+ var http = require("http")
+
+http.get(process.argv[2], processResponse);
+
+
+function processResponse(res) {
+  res.setEncoding("utf8")
+  res.on("data", console.log)
+  res.on("error", console.log)
+}
